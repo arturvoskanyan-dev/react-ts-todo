8c81d68e16d617e3a9d554640c66830935494de2
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-11-05 20:54:50 +0400

    chore: install testing libraries and setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // @ts-ignore
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
 })
